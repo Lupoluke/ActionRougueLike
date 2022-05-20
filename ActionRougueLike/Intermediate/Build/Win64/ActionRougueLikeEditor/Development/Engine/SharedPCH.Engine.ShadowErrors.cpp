266bfb9530ad7874a543a3e85108bfb500350831
Lupoluke <42489657+Lupoluke@users.noreply.github.com>
2022-05-20 13:31:03 +0200

    Added SCharacter and Added Forward&Turn Input
    Added Character Class
    Added MoveForward/Turn Input

---
+++ ActionRougueLike/Intermediate/Build/Win64/ActionRougueLikeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.32.31329
+#include "F:/ToolPerUnreal/GitHubRepository/ActionRougueLike/ActionRougueLike/Intermediate/Build/Win64/ActionRougueLikeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ ActionRougueLike/Intermediate/Build/Win64/UE4Editor/Inc/ActionRougueLike/ActionRougueLikeGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ACTIONROUGUELIKE_ActionRougueLikeGameModeBase_generated_h
+#error "ActionRougueLikeGameModeBase.generated.h already included, missing '#pragma once' in ActionRougueLikeGameModeBase.h"
+#endif
+#define ACTIONROUGUELIKE_ActionRougueLikeGameModeBase_generated_h
+
+#define ActionRougueLike_Source_ActionRougueLike_ActionRougueLikeGameModeBase_h_15_SPARSE_DATA
+#define ActionRougueLike_Source_ActionRougueLike_ActionRougueLikeGameModeBase_h_15_RPC_WRAPPERS
+#define ActionRougueLike_Source_ActionRougueLike_ActionRougueLikeGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define ActionRougueLike_Source_ActionRougueLike_ActionRougueLikeGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAActionRougueLikeGameModeBase(); \
+	friend struct Z_Construct_UClass_AActionRougueLikeGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AActionRougueLikeGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/ActionRougueLike"), NO_API) \
+	DECLARE_SERIALIZER(AActionRougueLikeGameModeBase)
+
+
+#define ActionRougueLike_Source_ActionRougueLike_ActionRougueLikeGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAActionRougueLikeGameModeBase(); \
+	friend struct Z_Construct_UClass_AActionRougueLikeGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AActionRougueLikeGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/ActionRougueLike"), NO_API) \
+	DECLARE_SERIALIZER(AActionRougueLikeGameModeBase)
+
+
+#define ActionRougueLike_Source_ActionRougueLike_ActionRougueLikeGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AActionRougueLikeGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AActionRougueLikeGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AActionRougueLikeGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AActionRougueLikeGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AActionRougueLikeGameModeBase(AActionRougueLikeGameModeBase&&); \
+	NO_API AActionRougueLikeGameModeBase(const AActionRougueLikeGameModeBase&); \
+public:
+
+
+#define ActionRougueLike_Source_ActionRougueLike_ActionRougueLikeGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AActionRougueLikeGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AActionRougueLikeGameModeBase(AActionRougueLikeGameModeBase&&); \
+	NO_API AActionRougueLikeGameModeBase(const AActionRougueLikeGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AActionRougueLikeGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AActionRougueLikeGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AActionRougueLikeGameModeBase)
+
+
+#define ActionRougueLike_Source_ActionRougueLike_ActionRougueLikeGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define ActionRougueLike_Source_ActionRougueLike_ActionRougueLikeGameModeBase_h_12_PROLOG
+#define ActionRougueLike_Source_ActionRougueLike_ActionRougueLikeGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ActionRougueLike_Source_ActionRougueLike_ActionRougueLikeGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	ActionRougueLike_Source_ActionRougueLike_ActionRougueLikeGameModeBase_h_15_SPARSE_DATA \
+	ActionRougueLike_Source_ActionRougueLike_ActionRougueLikeGameModeBase_h_15_RPC_WRAPPERS \
+	ActionRougueLike_Source_ActionRougueLike_ActionRougueLikeGameModeBase_h_15_INCLASS \
+	ActionRougueLike_Source_ActionRougueLike_ActionRougueLikeGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define ActionRougueLike_Source_ActionRougueLike_ActionRougueLikeGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ActionRougueLike_Source_ActionRougueLike_ActionRougueLikeGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	ActionRougueLike_Source_ActionRougueLike_ActionRougueLikeGameModeBase_h_15_SPARSE_DATA \
+	ActionRougueLike_Source_ActionRougueLike_ActionRougueLikeGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	ActionRougueLike_Source_ActionRougueLike_ActionRougueLikeGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	ActionRougueLike_Source_ActionRougueLike_ActionRougueLikeGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ACTIONROUGUELIKE_API UClass* StaticClass<class AActionRougueLikeGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID ActionRougueLike_Source_ActionRougueLike_ActionRougueLikeGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
